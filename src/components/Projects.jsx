45a8a06a61f360f6121aedf426156ffa1335de44
martaalcazarr <martaalcazarredondo@gmail.com>
2023-09-21 21:12:13 -0300

    add file only for icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,59 +1,10 @@
 import Chip from "@mui/material/Chip";
 import { Container, Typography, Grid, Paper, Button, Link } from "@mui/material";
 import { useLanguage } from "../context/languageContext";
-import {
-  SiHtml5,
-  SiCss3,
-  SiJavascript,
-  SiTailwindcss,
-  SiBootstrap, 
-  SiReact,
-  SiExpress, 
-  SiMongodb,
-  SiNodedotjs,
-  SiPug,
-  SiRuby,
-  SiRubyonrails,
-  SiPostgresql,
-  SiNextdotjs,
-  SiPrisma,
-  SiVite,
-  SiMui,
-  SiRemix,
-  SiStrapi,
-  SiSass,
-  SiGulp,
-  SiVuedotjs
-} from "react-icons/si";
-
+import techIcons from "./TechIcons";
 const Projects = () => {
   const { languageData } = useLanguage();
 
-  
-  const techIcons = {
-    HTML: <SiHtml5 />,
-    CSS: <SiCss3 />,
-    JavaScript: <SiJavascript />,
-    Tailwind: <SiTailwindcss />,
-    Bootstrap: <SiBootstrap />,
-    React: <SiReact />,
-    Express: <SiExpress />,
-    MongoDB: <SiMongodb />,
-    Node: <SiNodedotjs />,
-    Pug: <SiPug />,
-    Ruby: <SiRuby />,
-    RoR: <SiRubyonrails />,
-    PostgreSQL: <SiPostgresql />,
-    Next: <SiNextdotjs />,
-    Prisma: <SiPrisma />,
-    Vite: <SiVite />,
-    Mui: <SiMui />,
-    Remix: <SiRemix />,
-    Strapi: <SiStrapi />,
-    SASS: <SiSass />,
-    Gulp: <SiGulp />,
-    Vue: <SiVuedotjs />
-  };
 
   return (
     <Container>
